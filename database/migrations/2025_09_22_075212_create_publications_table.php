e2542953d7d3c5141827c21a8514d2fb6deb6e80
vincentiusantu <vincentiusantu@gmail.com>
2025-09-22 15:09:16 +0700

    chore: creating migrations

---
+++ database/migrations/2025_09_22_075212_create_publications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('publications', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('abstract');
+            $table->string('keywords');
+            $table->string('link');
+            $table->foreign('member_id')->reference('id')->on('members')->onDelete('cascade');
+            $table->string('category');
+            $table->string('publisher');
+            $table->string('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('publications');
+    }
+};
---
+++ database/migrations/2025_09_22_075224_create_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('role');
+            $table->string('email');
+            $table->string('major');
+            $table->string('department');
+            $table->string('photo');
+            $table->binary('isAlumni');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
